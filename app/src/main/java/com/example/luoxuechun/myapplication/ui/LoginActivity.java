9dc7a1344f72ecad9f7284719f6e49647b23955b
zhangchengyao <tc2002618@126.com>
2017-06-15 23:23:55 +0800

    小修改

--- app/src/main/java/com/example/luoxuechun/myapplication/ui/LoginActivity.java
+++ app/src/main/java/com/example/luoxuechun/myapplication/ui/LoginActivity.java
@@ -21,6 +21,8 @@ import android.widget.Toast;
 import com.example.luoxuechun.myapplication.R;
 import com.example.luoxuechun.myapplication.utils.LinkToServer;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 
@@ -73,13 +75,51 @@ public class LoginActivity extends BaseAppCompatActivity {
                 nameValue = et_name.getText().toString();
                 passValue = et_pass.getText().toString();
                 type = "tenant";
+
                 //check the right of user
                 String param = "{\"name\":"+"\""+nameValue+"\""+",\"password\":"+"\""+passValue+"\""+",\"type\":"+"\""+type+"\"}";
-                Log.v("param",param);
                 String result = LinkToServer.sendPost(url,param);
                 Log.v("loginact",result);
+
                 if(result!=null && result.length()!=0){
+                    //登录成功,获得该用户相关数据
+                    JSONObject json = null;
+                    String economic = null;
+                    String education = null;
+                    String gender = null;
+                    int id = 0;
+                    String name = null;
+                    String password = null;
+                    String phonenum = null;
+                    String preference = null;
+                    String vocation = null;
+                    try {
+                        json = new JSONObject(result);
+                        economic = json.getString("economic");
+                        education = json.getString("education");
+                        gender = json.getString("gender");
+                        id = json.getInt("id");
+                        name = json.getString("name");
+                        password = json.getString("password");
+                        phonenum = json.getString("phonenum");
+                        preference = json.getString("preference");
+                        vocation = json.getString("vocation");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Bundle data = new Bundle();
+                    data.putString("economic",economic);
+                    data.putString("education",education);
+                    data.putString("gender",gender);
+                    data.putInt("id",id);
+                    data.putString("name",name);
+                    data.putString("password",password);
+                    data.putString("phonenum",phonenum);
+                    data.putString("preference",preference);
+                    data.putString("vocation",vocation);
+
                     Intent intent=new Intent();
+                    intent.putExtras(data);
                     intent.setClass(LoginActivity.this,TenantOrderTempActivity.class);
                     startActivity(intent);
                 }else{
